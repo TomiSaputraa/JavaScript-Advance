e5e3bf6568d96f952f208b3c67638c9ac6de7d32
Tomi Saputra <tommycodingjj@gmail.com>
2023-11-24 01:40:06 +0700

    Destructuring Function

---
+++ 10. Destructuring Function.js
@@ -0,0 +1,65 @@
+// Destructuring Function
+
+// 1. Destructuring pada Objek dalam Fungsi:
+// Tanpa destructuring
+function printPerson(person) {
+  console.log(person.name);
+  console.log(person.age);
+}
+
+// Dengan destructuring
+function printPerson({ name, age }) {
+  console.log(name);
+  console.log(age);
+}
+
+const person = { name: "John", age: 30 };
+printPerson(person);
+
+// 2. Destructuring pada Array dalam Fungsi:
+
+// Tanpa Destructuring
+function kalkulasi(a, b) {
+  return [a + b, a * b];
+}
+
+// const jumlah = penjumlahanPerkalian(2, 3)[0];
+// const kali = penjumlahanPerkalian(2, 3)[1];
+
+// Dengan Destructuring
+const [jumlah, kali] = kalkulasi(2, 3);
+
+console.log(kali);
+
+// Atau
+
+// Tanpa destructuring
+function printNumbers(numbers) {
+  console.log(numbers[0]);
+  console.log(numbers[1]);
+}
+
+// Dengan destructuring
+function printNumbers([a, b]) {
+  console.log(a);
+  console.log(b);
+}
+
+const numbers = [1, 2];
+printNumbers(numbers);
+
+// 4. Contoh dengan object & function
+const mhs = {
+  nama: "Tomi",
+  umur: 23,
+  nilai: {
+    uas: 100,
+    uts: 80,
+  },
+};
+
+function cetakMhs({ nama, umur, nilai: { uas, uts } }) {
+  return `halo nama saya ${nama}, umur saya ${umur} tahun. nilai saya ${uts}`;
+}
+
+console.log(cetakMhs(mhs));
